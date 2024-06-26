fa7c19a9743ec964e08d6b446e0a291762a8e670
wk <howwhykid@gmail.com>
2024-06-26 03:38:54 -0400

    task2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,20 +2,26 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, setBudget } = useContext(AppContext);
+    const { budget, setBudget, expenses } = useContext(AppContext); // Assuming expenses is also provided by AppContext
     const [newBudget, setNewBudget] = useState(budget);
     const [error, setError] = useState('');
 
     const handleBudgetChange = (event) => {
         const value = parseInt(event.target.value, 10);
         const upperLimitBudget = 20000; // Cannot exceed 20000 pounds
-        
-        // budget - newBudget;
+
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
 
         if (!isNaN(value)) {
             if (value > upperLimitBudget) {
-                // Show alert when value exceeds remaining funds
-                alert(`The value cannot exceed remaining funds of: £${upperLimitBudget}`);
+                // Show alert when value exceeds upper limit
+                alert(`The value cannot exceed £${upperLimitBudget}`);
+                setNewBudget(newBudget); // Reset input value to current newBudget
+            } else if (value < totalExpenses) {
+                // Show alert when value is lower than totalExpenses
+                alert('You cannot reduce the budget value lower than the expenses');
                 setNewBudget(newBudget); // Reset input value to current newBudget
             } else {
                 setNewBudget(value);
